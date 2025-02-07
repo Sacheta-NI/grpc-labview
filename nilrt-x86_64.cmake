675ce16b74c8c4b011fad9b1be494a449e41fdb1
Sacheta . <sacheta@ni.com>
2025-02-07 14:24:39 +0530

    making changes linux rt gcc
    Signed-off-by: Sacheta . <sacheta@ni.com>

--- nilrt-x86_64.cmake
+++ nilrt-x86_64.cmake
@@ -15,8 +15,8 @@ set(include_path core2-64-nilrt-linux/usr/include/c++/6.3.0)
 #----------------------------------------------------------------------
 # Compilers
 #----------------------------------------------------------------------
-set(CMAKE_C_COMPILER x86_64-nilrt-linux-gcc)
-set(CMAKE_CXX_COMPILER x86_64-nilrt-linux-g++)
+set(CMAKE_C_COMPILER x86_64-nilrt-linux-gcc-10)
+set(CMAKE_CXX_COMPILER x86_64-nilrt-linux-g++-10)
 
 #----------------------------------------------------------------------
 # Default compiler flags
